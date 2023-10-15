2e96146695360f4a9e1924f5416e4af7cbdbe47e
Rustam Saitov <78560640+strrust@users.noreply.github.com>
2023-10-15 22:44:03 +0300

    [HW-3] TAF skeleton
    RequestSpecificationFactory
    api properties template updates
    readme updates

--- README.md
+++ README.md
@@ -13,7 +13,11 @@ This repository contains tasks for TA GMP.
 
 * Run ReportPortal locally with Docker
 
-* To run all tests, use command:
+* To run all api-tests, use command:
 
-> mvn clean test -DTA_ENV=local -DlogLevel=INFO
-> 
+> mvn clean test -pl -api-tests -DTA_ENV=local -DlogLevel=INFO -DAUTH_TOKEN={RP_API_key}
+>
+
+* To run all ui-tests, use command:
+
+> mvn clean test -pl -ui-tests -DTA_ENV=local -DlogLevel=INFO
---
+++ api-tests/src/main/java/com/gmp/reportportal/api/RequestSpecificationFactory.java
@@ -0,0 +1,30 @@
+package com.gmp.reportportal.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.LogConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.http.ContentType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import static io.restassured.filter.log.LogDetail.ALL;
+
+@Component
+public class RequestSpecificationFactory {
+    @Value("${backend.baseUrl}")
+    private String baseUri;
+    @Value("${backend.basePath}")
+    private String basePath;
+    @Value("${AUTH_TOKEN}")
+    private String authToken;
+
+    private RequestSpecBuilder getSpecificationBuilder() {
+        return new RequestSpecBuilder()
+                .addHeader("Authorization", String.format("Bearer %s", authToken))
+                .setConfig(RestAssuredConfig.config().logConfig(LogConfig.logConfig().blacklistHeader("Authorization")))
+                .setBaseUri(baseUri)
+                .setBasePath(basePath)
+                .log(ALL)
+                .setContentType(ContentType.JSON);
+    }
+}
