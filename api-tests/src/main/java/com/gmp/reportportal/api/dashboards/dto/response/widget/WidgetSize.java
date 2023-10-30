8904cc868834cc9335fe8ace60f25515448bbedf
Rustam Saitov <78560640+strrust@users.noreply.github.com>
2023-10-30 17:37:27 +0300

    [HW-4] Test Runners
    duplicated dto removed

--- api-tests/src/main/java/com/gmp/reportportal/api/dashboards/dto/response/widget/WidgetSize.java
+++
@@ -1,9 +0,0 @@
-package com.gmp.reportportal.api.dashboards.dto.response.widget;
-
-import lombok.Getter;
-
-@Getter
-public class WidgetSize {
-    private Integer width;
-    private Integer height;
-}
