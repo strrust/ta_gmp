17a6b88d20ef905835995348d4e62ead708e9a36
Rustam Saitov <78560640+strrust@users.noreply.github.com>
2023-11-20 01:07:36 +0300

    [HW-5] BDD
    ScenarioContext

--- bdd-tests/src/test/java/com/gmp/reportportal/configuration/TestContextConfiguration.java
+++ bdd-tests/src/test/java/com/gmp/reportportal/configuration/TestContextConfiguration.java
@@ -1,7 +1,27 @@
 package com.gmp.reportportal.configuration;
 
-import org.springframework.context.annotation.Configuration;
+import com.gmp.reportportal.steps.ScenarioContext;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.SimpleThreadScope;
 
 @Configuration
+@ComponentScan({"com.gmp.reportportal"})
+@PropertySources({
+        @PropertySource("classpath:env/${TA_ENV:local}.properties")
+})
 public class TestContextConfiguration {
+
+    @Bean
+    public static CustomScopeConfigurer threadLocalScopeRegistration() {
+        CustomScopeConfigurer scopeConfigurator = new CustomScopeConfigurer();
+        scopeConfigurator.addScope("threadlocal", new SimpleThreadScope());
+        return scopeConfigurator;
+    }
+
+    @Bean
+    @Scope("threadlocal")
+    public ScenarioContext scenarioContext() {
+        return new ScenarioContext();
+    }
 }
--- bdd-tests/src/test/java/com/gmp/reportportal/steps/DashboardsTableSteps.java
+++ bdd-tests/src/test/java/com/gmp/reportportal/steps/DashboardsTableSteps.java
@@ -5,27 +5,26 @@ import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class DashboardsTableSteps {
-
     @Autowired
-    ScenarioContext scenarioContext;
+    private ScenarioContext scenarioContext;
 
     @When("I load dashboards")
     public void iLoadDashboards() {
-
+        scenarioContext.dashboardsTable.loadDashboardsTable();
     }
 
     @When("I open demo dashboard")
     public void iOpenDemoDashboard() {
-
+        // TODO
     }
 
     @Then("Dashboard is opened")
     public void dashboardIsOpened() {
-
+        // TODO
     }
 
     @Then("Dashboard contains widgets")
     public void dashboardContainsWidgets() {
-
+        // TODO
     }
 }
