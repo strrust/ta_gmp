e6440d97a2522e10e34208b86181ad91d1087297
Rustam Saitov <78560640+strrust@users.noreply.github.com>
2023-11-30 11:09:26 +0300

    [HW-5] BDD
    cucumber hooks

--- bdd-tests/src/test/java/com/gmp/reportportal/configuration/CucumberHooks.java
+++ bdd-tests/src/test/java/com/gmp/reportportal/configuration/CucumberHooks.java
@@ -3,17 +3,24 @@ package com.gmp.reportportal.configuration;
 import com.gmp.reportportal.domain.dashboards.DashboardsTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
+@Slf4j
 @CucumberContextConfiguration
 @ContextConfiguration(classes = {TestContextConfiguration.class})
 public class CucumberHooks {
     @Autowired
     private DashboardsTable dashboardsTable;
+
+    private static int failedTests = 0;
+    private static int passedTests = 0;
+    private static int count = 0;
 
     @After("@widget")
     public void removeLastCreatedWidget() {
@@ -22,11 +29,24 @@ public class CucumberHooks {
 
     @Before("@filter")
     public void createAdditionalDesignBeforeFiltering() {
-        dashboardsTable.createDashboard("Additional dashboard", EMPTY);
+        dashboardsTable.createDashboard("ta-db-1", EMPTY);
     }
 
     @After("@filter")
     public void removeAdditionalDesignAfterFiltering() {
-        dashboardsTable.removeDashboard("Additional dashboard");
+        dashboardsTable.loadDashboardsTable();
+        dashboardsTable.removeDashboard("ta-db-1");
+    }
+
+    @After
+    public void logCountOfTest(Scenario scenario) {
+        count++;
+        if (scenario.isFailed()) {
+            failedTests++;
+        } else {
+            passedTests++;
+        }
+        log.info("Tests completed: {}", count);
+        log.info("Passed: {}, Failed: {}", passedTests, failedTests);
     }
 }
